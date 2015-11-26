2fb3546294267776aebb5dac5e69cecbbd65b7b4
Eric1994 <eric.ramos.suarez@gmail.com>
2015-11-26 17:20:15 +0000

    Cambios

--- archivo.pl
+++ archivo.pl
@@ -7,3 +7,13 @@ before(X, Y, [_|T]) :-
 conc([], L, L).
 
 conc([X|L1], L2, [X|L3]) :- conc(L1, L2, L3). 
+
+fact(1,1) :- !.
+fact(N,F) :- 
+	N > 0,
+    N1 is N - 1, 
+    fact(N1, F1), 
+    F is N * F1,
+    !.
+% el !. es para que No se quede trabado
+
